e1c4029711352dff6e1fcb597a5e3f48d0d402c3
cd-Roid <giyawe98@gmail.com>
2022-06-11 17:52:30 +0200

    use tab and Category

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,40 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import kacheln from '../../json/kacheln.json';
 import Category from './Category';
+import Tab from './Tab';
 
 type SidebarProps = {};
 
 const Sidebar: React.FC<SidebarProps> = () => {
-  const categories = kacheln.map((c) => <Category item={c} />);
+  const [toggleState, setToggleState] = useState(0);
+
+  const toggleTab = (index: number) => {
+    setToggleState(index);
+  };
+
   return (
-    <div className="absolute px-4 bg-gray-100 w-screen h-24 bottom-0">
-      {categories}
+    <div className="absolute px-4 bg-gray-100 w-screen h-24 bottom-0 container">
+      <div className="bloc-tabs">
+        {kacheln.map((a, index) => (
+          <Tab
+            key={index}
+            tabIndex={index}
+            tabText={a.category}
+            onClickAction={() => toggleTab(index)}
+          />
+        ))}
+      </div>
+      <div className="content-tabs">
+        {kacheln.map((c, index) => (
+          <div
+            className={
+              toggleState === index ? 'content  active-content' : 'content'
+            }
+          >
+            <Category items={c} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
