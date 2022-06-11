c3fb5ed9bfef2b4d79e7cf222fc92fff24867797
cd-Roid <giyawe98@gmail.com>
2022-06-11 17:51:38 +0200

    deleted unused css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App h-screen w-screen bg-gray-600">
+    <div className=" h-screen w-screen bg-gray-600">
       <Sidebar />
     </div>
   );
