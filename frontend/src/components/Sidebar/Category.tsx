5a85aaff9fac65cb233de06f94f8eca96f2b65f0
cd-Roid <giyawe98@gmail.com>
2022-06-11 18:59:05 +0200

    added better types to Category

--- frontend/src/components/Sidebar/Category.tsx
+++ frontend/src/components/Sidebar/Category.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Tile from '../Tile/Tile';
 
-type Props = {
-  items: {};
+type CategoryProps = {
+  items: {
+    category: string;
+    items: {
+      name: string;
+    }[];
+  };
+  onClickAction: () => void;
 };
 
-const Category: React.FC<Props> = ({ items }) => {
-  return <div className="content-tabs">{JSON.stringify(items)}</div>;
+const Category: React.FC<CategoryProps> = ({ items, onClickAction }) => {
+  return (
+    <div
+      className="content-tabs flex justify-between p-4"
+      onClick={onClickAction}
+    >
+      {items.items.map((a) => (
+        <Tile name={a.name} category={items.category} />
+      ))}
+    </div>
+  );
 };
 
 
