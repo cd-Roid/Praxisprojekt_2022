5de3918afd9c43e57358a5f25ce53e5f980ebf15
cd-Roid <giyawe98@gmail.com>
2022-06-19 19:55:09 +0200

    used Draggable

--- frontend/src/components/Tile/Tile.tsx
+++ frontend/src/components/Tile/Tile.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import Draggable from 'react-draggable';
 type TileProps = {
   name: string;
   category: string;
 };
 
 const Tile: React.FC<TileProps> = ({ name, category }) => {
-  return <div className={`${category} flex justify-center items-center m-4 `}>{name}</div>;
+  return (
+    <Draggable>
+      <div className={`${category} flex justify-center items-center m-4 `}>{name}</div>
+    </Draggable>
+  );
 };
 
 export default Tile;
