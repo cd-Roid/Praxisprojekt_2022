254151ae3a130cc62c0918b3fcdf8636feaffaa3
cd-Roid <giyawe98@gmail.com>
2022-06-19 21:36:25 +0200

    cleanup tile

--- frontend/src/components/Tile/Tile.tsx
+++ frontend/src/components/Tile/Tile.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import Draggable from 'react-draggable';
 type TileProps = {
   name: string;
   category: string;
 };
 
 const Tile: React.FC<TileProps> = ({ name, category }) => {
-  return (
-    <Draggable>
-      <div className={`${category} flex justify-center items-center m-4 `}>{name}</div>
-    </Draggable>
-  );
+  return <div className={`${category} flex justify-center items-center m-4 `}>{name}</div>;
 };
 
 export default Tile;
