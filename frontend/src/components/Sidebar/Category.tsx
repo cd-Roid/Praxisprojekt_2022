5b6fcf024bd87bf799a3887dbabc1607700640e8
cd-Roid <giyawe98@gmail.com>
2022-06-19 21:36:03 +0200

    added draggable component

--- frontend/src/components/Sidebar/Category.tsx
+++ frontend/src/components/Sidebar/Category.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Draggable from 'react-draggable';
 import Tile from '../Tile/Tile';
 
 type CategoryProps = {
@@ -12,14 +13,25 @@ type CategoryProps = {
 };
 
 const Category: React.FC<CategoryProps> = ({ items, onClickAction }) => {
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    if (dragging === true) {
+      console.log('Should create a new Element copy');
+    }
+  }, []);
+
   return (
     <div className='content-tabs  inline-flex' onClick={onClickAction}>
       {items.items.map((a, index) => (
-        <Tile key={index} name={a.name} category={items.category} />
+        <Draggable key={index} onStart={() => setDragging(true)} onStop={() => setDragging(false)}>
+          <div>
+            <Tile key={index} name={a.name} category={items.category} />
+          </div>
+        </Draggable>
       ))}
     </div>
   );
 };
-
 
 export default Category;
