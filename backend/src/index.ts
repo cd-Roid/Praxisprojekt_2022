f42298b6a385537905f6b993c0cd93690b767d60
George Iyawe <george.iyawe@kernpunkt.de>
2022-07-15 00:27:11 +0200

    handle connections

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const server = express();
+server.use(cors({ origin: "localhost:9001", credentials: true }));
 const port = 9000;
-const io = new Server(9001);
+const io = new Server();
 
 server.get("/", (req: express.Request, res: express.Response) =>
 	res.send("Hello from Server" + port),
@@ -12,7 +14,8 @@ server.get("/", (req: express.Request, res: express.Response) =>
 io.sockets.on("connection", () => console.log("Listening for connections"));
 
 io.on("connection", (socket) => {
-	console.log(`a user connected ${socket.data}`);
+	console.log(`a user connected ${socket.id}`);
 });
 
+io.listen(9001);
 server.listen(port, () => console.log(`Server running on port  ${port}`));
