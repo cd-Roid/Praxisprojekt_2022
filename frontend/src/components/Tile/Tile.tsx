1584300f345e401c15b6a1a041edabe48e78a6e7
George Iyawe <george.iyawe@kernpunkt.de>
2022-07-27 16:53:53 +0200

    added optional style prop

--- frontend/src/components/Tile/Tile.tsx
+++ frontend/src/components/Tile/Tile.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+
 type TileProps = {
   name: string;
   category: string;
+  styles?: object;
 };
 
-const Tile: React.FC<TileProps> = ({ name, category }) => {
-  return <div className={`${category} flex justify-center items-center `}>{name}</div>;
+const Tile: React.FC<TileProps> = ({ name, category, styles }) => {
+  return (
+    <div style={styles} className={`${category} flex justify-center items-center `}>
+      {name}
+    </div>
+  );
 };
 
 export default Tile;
