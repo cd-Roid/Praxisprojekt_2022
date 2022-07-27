7e9c54df39f5d29c9aa0e9949e880e05fb4627fe
George Iyawe <george.iyawe@kernpunkt.de>
2022-07-27 16:53:41 +0200

    added custom types

---
+++ frontend/src/types.ts
@@ -0,0 +1,13 @@
+export type NewNode = {
+  className: string;
+  name: string;
+  position: {
+    x: number;
+    y: number;
+  };
+};
+
+export type CursorData = {
+  x: number;
+  y: number;
+};
