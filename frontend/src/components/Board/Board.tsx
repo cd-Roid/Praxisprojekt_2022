275c07472f05dbaaabe2ae7b2a3dc26e20e83238
cd-Roid <giyawe98@gmail.com>
2022-08-15 20:51:10 +0200

    added missing deps

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import Konva from 'konva';
+import { Vector2d } from 'konva/lib/types';
+import React, { useEffect, useState } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 import { NewNode } from '../../types';
@@ -9,26 +11,69 @@ import Tile from '../Tile/Tile';
 const Board = () => {
   const { height, width } = useWindowDimensions();
   const [tilesOnBoard, setTilesOnBoard] = useState<NewNode[]>([]);
-
+  const [cursorPos, setCursorPos] = useState<Vector2d | null>();
+  const stageRef = React.useRef<Konva.Stage>(null);
   const handleDragOver = (event: React.DragEvent) => event.preventDefault();
 
   const handleDrop = (event: React.DragEvent) => {
     const draggedData = event.dataTransfer.getData('dragStart/Tile');
+    console.log(cursorPos?.x, cursorPos?.y);
     console.log(draggedData);
-    if (draggedData) {
-      const { name, nodeClass, offsetX, offsetY, posX, posY } = JSON.parse(draggedData);
-      const newTile: NewNode = {
-        name: name,
-        category: nodeClass,
-        x: posX - offsetX,
-        y: posY - offsetY,
-      };
-      setTilesOnBoard(tilesOnBoard.concat(newTile));
+    if (draggedData && stageRef.current != null) {
+      stageRef.current.setPointersPositions(event);
+      const coordinates = stageRef.current.getPointerPosition();
+      const { name, nodeClass, offsetX, offsetY } = JSON.parse(draggedData);
+      if (coordinates != undefined) {
+        const newTile: NewNode = {
+          name: name,
+          category: nodeClass,
+          x: coordinates?.x - offsetX,
+          y: coordinates?.y - offsetY,
+        };
+        setTilesOnBoard(tilesOnBoard.concat(newTile));
+      }
     }
   };
+  const handleMouseMove = () => {
+    if (stageRef.current != null) {
+      setCursorPos(stageRef.current.getPointerPosition());
+    }
+  };
+
   return (
-    <main onDrop={handleDrop} onDragOver={handleDragOver}>
-      <Stage width={width} height={height}>
+    <main onDrop={handleDrop} onDragOver={handleDragOver} onMouseMove={handleMouseMove}>
+      <div>
+        {cursorPos?.x},{cursorPos?.y}
+        <Stage width={width} height={height} ref={stageRef}>
+          <Layer>
+            {tilesOnBoard.map((tile, index) => {
+              return (
+                <Line
+                  key={index}
+                  draggable
+                  x={tile.x}
+                  y={tile.y}
+                  points={[0, 0, 100, 0, 100, 100]}
+                  tension={0.5}
+                  closed
+                  stroke='black'
+                  fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+                  fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+                  fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+                />
+              );
+            })}
+          </Layer>
+        </Stage>
+      </div>
+    </main>
+  );
+};
+
+export default Board;
+
+/**
+ *     <Stage width={width} height={height}>
         <Layer>
           {tilesOnBoard.map((tile, index) => {
             <Line
@@ -43,20 +88,6 @@ const Board = () => {
               fillLinearGradientEndPoint={{ x: 50, y: 50 }}
               fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
             />;
-          })}
-        </Layer>
-      </Stage>
-    </main>
-  );
-};
-
-export default Board;
-
-/**
- *    <Stage width={width} height={height}>
-        <Layer>
-          {tilesOnBoard.map((tile, index) => {
-            <Tile key={index} name={tile.name} category={tile.category} />;
           })}
         </Layer>
       </Stage>
