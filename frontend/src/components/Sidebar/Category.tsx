fda03a7a3e6064544dcb026dcad208033f9fb315
cd-Roid <giyawe98@gmail.com>
2022-08-15 20:16:35 +0200

    added base logic of drag

--- frontend/src/components/Sidebar/Category.tsx
+++ frontend/src/components/Sidebar/Category.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import data from '../../json/kacheln.json';
-import { SocketProvider } from '../../providers/SocketProvider';
 import Tile from '../Tile/Tile';
 
 type InnerObject = {
@@ -14,49 +13,42 @@ type CategoryProps = {
 
 const Category: React.FC<CategoryProps> = ({ category }) => {
   const [stateItems, setStateItems] = useState<InnerObject[]>([]);
-  const { currentSocket } = SocketProvider();
 
-  const onDragStart = (event: React.DragEvent, nodeName: string, nodeClass: string) => {
-    localStorage.setItem('application/TileName', nodeName);
-    localStorage.setItem('application/TileClass', nodeClass);
-    event.dataTransfer.setData('application/TileName', nodeName);
-    event.dataTransfer.setData('application/TileClass', nodeClass);
-    event.dataTransfer.effectAllowed = 'move';
-  };
+  const handleDragStart = (event: React.DragEvent) => {
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
 
-  const drag = (event: React.DragEvent) => {
-    const nodeName = localStorage.getItem('application/TileName');
-    const nodeClass = localStorage.getItem('application/TileClass');
-    const node = {
-      name: nodeName,
-      class: nodeClass,
-      position: {
-        x: event.pageX,
-        y: event.pageY,
-      },
-    };
-    currentSocket?.emit('node-dragging', node);
-  };
-  useEffect(() => {
-    const a: InnerObject[] = [];
-    data.forEach((object) => {
-      object.category === category && a.push(object);
+    const dragPayload = JSON.stringify({
+      name: event.currentTarget.getAttribute('data-name'),
+      nodeClass: event.currentTarget.getAttribute('data-class'),
+      offsetX: offsetX,
+      offsetY: offsetY,
     });
-    setStateItems(a);
+
+    event.dataTransfer.setData('dragStart/Tile', dragPayload);
+  };
+
+  useEffect(() => {
+    const InnerObjectArray: InnerObject[] = [];
+    data.forEach((object) => {
+      object.category === category && InnerObjectArray.push(object);
+    });
+    setStateItems(InnerObjectArray);
   }, []);
 
   return (
     <div className='content-tabs  inline-flex'>
       {stateItems &&
-        stateItems.map((a, index: number) => (
+        stateItems.map((tile, index: number) => (
           <div
+            data-name={tile.name}
+            data-class={category}
             className={`${category} m-4`}
             key={index}
-            onDragStart={(e) => onDragStart(e, a.name, a.category)}
-            onDrag={drag}
             draggable
+            onDragStart={(e) => handleDragStart(e)}
           >
-            <Tile key={index} name={a.name} category={category} />
+            <Tile key={index} name={tile.name} category={category} />
           </div>
         ))}
     </div>
