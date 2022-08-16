2f5c6a4d165a6e4322c89f959e702633c80532ba
cd-Roid <giyawe98@gmail.com>
2022-08-16 16:07:52 +0200

    moved functions to own hook

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,16 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Konva from 'konva';
-import { Vector2d } from 'konva/lib/types';
 import React, { useState, useRef, useEffect } from 'react';
-import { Stage, Layer, Group, Text, RegularPolygon } from 'react-konva';
+import { Stage, Layer } from 'react-konva';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 import { NewNode } from '../../types';
 import Tile from '../Tiles/Tile';
+import { handleDragOver, handleDrop } from '../../hooks/useDrag';
 
 // Main Stage Component that holds the Canvas. Scales based on the window size.
-
-// TODO: move all Shape Logic to own File
-// TODO: maybe move drag Logic to own hook
 
 const Board = () => {
   const textRef = useRef<Konva.Text>();
@@ -26,39 +23,14 @@ const Board = () => {
 
   const { height, width } = useWindowDimensions();
   const [tilesOnBoard, setTilesOnBoard] = useState<NewNode[]>([]);
-  const [cursorPos, setCursorPos] = useState<Vector2d | null>();
   const stageRef = React.useRef<Konva.Stage>(null);
-  const handleDragOver = (event: React.DragEvent) => event.preventDefault();
-
-  const handleDrop = (event: React.DragEvent) => {
-    const draggedData = event.dataTransfer.getData('dragStart/Tile');
-    if (draggedData && stageRef.current != null) {
-      stageRef.current.setPointersPositions(event);
-      const coordinates = stageRef.current.getPointerPosition();
-      const { name, nodeClass, offsetX, offsetY } = JSON.parse(draggedData);
-      if (coordinates != undefined) {
-        const newTile: NewNode = {
-          name: name,
-          category: nodeClass,
-          x: coordinates?.x - offsetX,
-          y: coordinates?.y - offsetY,
-        };
-        console.log(offsetX, offsetY);
-        setTilesOnBoard(tilesOnBoard.concat(newTile));
-      }
-    }
-  };
-
-  const handleMouseMove = () => {
-    if (stageRef.current != null) {
-      setCursorPos(stageRef.current.getPointerPosition());
-    }
-  };
 
   return (
-    <main onDrop={handleDrop} onDragOver={handleDragOver} onMouseMove={handleMouseMove}>
+    <main
+      onDrop={(e) => handleDrop(e, tilesOnBoard, stageRef, setTilesOnBoard)}
+      onDragOver={handleDragOver}
+    >
       <div>
-        {cursorPos?.x},{cursorPos?.y}
         <Stage width={width} height={height} ref={stageRef}>
           <Layer>
             {tilesOnBoard.map((tile, index) => {
