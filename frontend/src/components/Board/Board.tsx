4856e80483f8388354b4280b141020295d087ab4
cd-Roid <giyawe98@gmail.com>
2022-08-16 18:15:07 +0200

    cleanup

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,5 +1,5 @@
 import Konva from 'konva';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Stage, Layer } from 'react-konva';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 import { NewNode } from '../../types';
@@ -9,17 +9,6 @@ import { handleDragOver, handleDrop } from '../../hooks/useDrag';
 // Main Stage Component that holds the Canvas. Scales based on the window size.
 
 const Board = () => {
-  const textRef = useRef<Konva.Text>();
-  const [size, setSize] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-  useEffect(() => {
-    if (textRef.current != undefined) {
-      setSize({
-        x: textRef.current.width(),
-        y: textRef.current.height(),
-      });
-    }
-  }, []);
-
   const { height, width } = useWindowDimensions();
   const [tilesOnBoard, setTilesOnBoard] = useState<NewNode[]>([]);
   const stageRef = React.useRef<Konva.Stage>(null);
@@ -34,16 +23,7 @@ const Board = () => {
           <Layer>
             {tilesOnBoard.map((tile, index) => {
               return (
-                <Tile
-                  key={index}
-                  category={tile.category}
-                  name={tile.name}
-                  sizeX={size.x}
-                  sizeY={size.y}
-                  textRef={textRef}
-                  x={tile.x}
-                  y={tile.y}
-                />
+                <Tile key={index} category={tile.category} name={tile.name} x={tile.x} y={tile.y} />
               );
             })}
           </Layer>
