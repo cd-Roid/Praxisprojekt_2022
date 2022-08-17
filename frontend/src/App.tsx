46948a4a88f1e9d684d2d4dc9f2d85d70a78242a
cd-Roid <giyawe98@gmail.com>
2022-08-17 19:40:49 +0200

    added Function to Add new Tiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './components/Board/Board';
+import NewTileButton from './components/Buttons/NewTileButton';
 import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
     <>
+      <NewTileButton />
       <Board />
       <Sidebar />
     </>
