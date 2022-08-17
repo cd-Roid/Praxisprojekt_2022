b17d5a255ab02011e93f3c70cdfe4c2028cf27ee
cd-Roid <giyawe98@gmail.com>
2022-08-17 20:01:43 +0200

    reverted bad change

--- frontend/src/components/Forms/AddTileForm.tsx
+++ frontend/src/components/Forms/AddTileForm.tsx
@@ -18,6 +18,7 @@ const AddTileForm: React.FC<AddTileFormProps> = ({ closeForm }) => {
     setIsOpen(false);
   };
 
+  // TODO: Add a database to save the new Tiles to.
   const handleSubmit = () => {
     selectedName &&
       selectedCategory &&
@@ -45,61 +46,59 @@ const AddTileForm: React.FC<AddTileFormProps> = ({ closeForm }) => {
   }, []);
 
   return (
-    <div className='w-screen h-screen' onClick={closeForm}>
-      <div className='w-full max-w-xs'>
-        <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-          <div className='mb-4'>
-            <label className='block text-gray-700 text-sm font-bold mb-2'>Tile Name</label>
-            <input
-              onChange={handleChange}
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='tileName'
-              type='text'
-              placeholder='Name'
-            />
-          </div>
-          <div className='mb-6'>
-            <label className='block text-gray-700 text-sm font-bold mb-2'>Category</label>
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              id='dropdownDefault'
-              data-dropdown-toggle='dropdown'
-              className='w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center  justify-between'
-              type='button'
+    <div className='w-full max-w-xs'>
+      <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+        <div className='mb-4'>
+          <label className='block text-gray-700 text-sm font-bold mb-2'>Tile Name</label>
+          <input
+            onChange={handleChange}
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='tileName'
+            type='text'
+            placeholder='Name'
+          />
+        </div>
+        <div className='mb-6'>
+          <label className='block text-gray-700 text-sm font-bold mb-2'>Category</label>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            id='dropdownDefault'
+            data-dropdown-toggle='dropdown'
+            className='w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center  justify-between'
+            type='button'
+          >
+            {categories && selectedCategory ? selectedCategory : categories[0]}
+            <FontAwesomeIcon icon={isOpen ? faAngleUp : faAngleDown} />
+          </button>
+          {isOpen && (
+            <div
+              id='dropdown'
+              className='w-full z-10  bg-green-600 rounded-b-lg divide-y divide-gray-100 shadow'
             >
-              {categories && selectedCategory ? selectedCategory : categories[0]}
-              <FontAwesomeIcon icon={isOpen ? faAngleUp : faAngleDown} />
-            </button>
-            {isOpen && (
-              <div
-                id='dropdown'
-                className='w-full z-10  bg-green-600 rounded-b-lg divide-y divide-gray-100 shadow'
+              <ul
+                className='py-1 text-sm text-gray-700 dark:text-gray-200'
+                aria-labelledby='dropdownDefault'
               >
-                <ul
-                  className='py-1 text-sm text-gray-700 dark:text-gray-200'
-                  aria-labelledby='dropdownDefault'
-                >
-                  {categories &&
-                    categories.map((category, index) => (
-                      <li key={index} onClick={() => setCategory(category)}>
-                        {category}
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            )}
-          </div>
-          <div className='flex items-center justify-between'>
-            <button
-              onClick={handleSubmit}
-              className='bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded'
-              type='button'
-            >
-              Add Tile
-            </button>
-          </div>
-        </form>
-      </div>
+                {categories &&
+                  categories.map((category, index) => (
+                    <li key={index} onClick={() => setCategory(category)}>
+                      {category}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className='flex items-center justify-between'>
+          <button
+            onClick={handleSubmit}
+            className='bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded'
+            type='button'
+          >
+            Add Tile
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
