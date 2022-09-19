9e5cd02f3287f8b1eca2046bd6dcd6669519e48c
cd-Roid <giyawe98@gmail.com>
2022-09-19 18:35:28 +0200

    added updateTilePosition function

--- frontend/src/hooks/useMouse.tsx
+++ frontend/src/hooks/useMouse.tsx
@@ -5,67 +5,56 @@ import { KonvaEventObject } from 'konva/lib/Node';
 import { useBoardState } from '../state/BoardContext';
 
 export const useMouse = () => {
-  const tilesOnBoard = useBoardState((state) => state.tilesOnBoard);
-  const setTiles = useBoardState((state) => state.addTile);
-  const updateTile = useBoardState((state) => state.updateTile);
-  const stageRef = useBoardState((state) => state.stageReference);
+    const tilesOnBoard = useBoardState((state) => state.tilesOnBoard);
+    const setTiles = useBoardState((state) => state.addTile);
+    const updateTile = useBoardState((state) => state.updateTile);
+    const stageRef = useBoardState((state) => state.stageReference);
 
-  const handleDragOver = (event: React.DragEvent) => event.preventDefault();
+    const handleDragOver = (event: React.DragEvent) => event.preventDefault();
 
-  const handleDragStart = (event: React.DragEvent) => {
-    const dragPayload = JSON.stringify({
-      name: event.currentTarget.getAttribute('data-name'),
-      nodeClass: event.currentTarget.getAttribute('data-class'),
-    });
-    event.dataTransfer.setData('dragStart/Tile', dragPayload);
-  };
+    const handleDragStart = (event: React.DragEvent) => {
+      const dragPayload = JSON.stringify({
+        name: event.currentTarget.getAttribute('data-name'),
+        nodeClass: event.currentTarget.getAttribute('data-class'),
+      });
+      event.dataTransfer.setData('dragStart/Tile', dragPayload);
+    };
 
-  const updateTilePosition = (event: KonvaEventObject<DragEvent>) => {
-    console.log(event.currentTarget);
-    if (stageRef.current) {
-      const coordinates = stageRef.current.getRelativePointerPosition();
-      // update the tile position
-      /**
- *       if (coordinates) {
-        const newTiles = tilesOnBoard.map((tile) => {
-          if (tile.id === event.target.id()) {
-            return {
-              ...tile,
-              x: coordinates.x,
-              y: coordinates.y,
-            };
-          }
-          return tile;
-        });
-        console.log(newTiles);
-
- *  */
-    }
-  };
-
-  const handleDrop = (
-    event: React.DragEvent,
-    tilesOnBoard: NewNode[],
-    setTilesOnBoard: React.Dispatch<React.SetStateAction<NewNode[]>>,
-  ) => {
-    event.preventDefault();
-    const draggedData = event.dataTransfer.getData('dragStart/Tile');
-    if (draggedData && stageRef.current != null) {
-      stageRef.current.setPointersPositions(event);
-      const coordinates = stageRef.current.getRelativePointerPosition();
-      const { name, nodeClass } = JSON.parse(draggedData);
-      if (coordinates != undefined) {
-        const newTile: NewNode = {
-          id: uuidv4(),
-          name: name,
-          category: nodeClass,
-          x: coordinates?.x,
-          y: coordinates?.y,
+    const updateTilePosition = (event: KonvaEventObject<DragEvent>) => {
+      const { text } = event.target.getAttr('children')[1].attrs;
+      if (stageRef.current) {
+        const { x, y } = stageRef.current.getRelativePointerPosition();
+        console.log(event.target.attrs.name);
+        const updatedTile: NewNode = {
+          id: event.target.attrs.id,
+          name: text,
+          category: event.target.attrs.name,
+          x: x,
+          y: y,
         };
-        setTilesOnBoard([...tilesOnBoard, newTile]);
+        updateTile(updatedTile);
       }
-    }
-  };
+    };
+
+    const handleDrop = (event: React.DragEvent) => {
+      event.preventDefault();
+      const draggedData = event.dataTransfer.getData('dragStart/Tile');
+      if (draggedData && stageRef.current != null) {
+        stageRef.current.setPointersPositions(event);
+        const coordinates = stageRef.current.getRelativePointerPosition();
+        const { name, nodeClass } = JSON.parse(draggedData);
+        if (coordinates != undefined) {
+          const newTile: NewNode = {
+            id: uuidv4(),
+            name: name,
+            category: nodeClass,
+            x: coordinates?.x,
+            y: coordinates?.y,
+          };
+          setTiles(newTile);
+        }
+      }
+    };
 
   const handleWheel = (event: KonvaEventObject<WheelEvent>) => {
     event.evt.preventDefault();
