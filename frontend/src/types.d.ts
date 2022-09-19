d29031cf70eca86148c736d681f92be69c3ce6b9
cd-Roid <giyawe98@gmail.com>
2022-09-19 20:26:34 +0200

    added actual type file

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,30 @@
+export type NewNode = {
+  id: string;
+  category: string;
+  name: string;
+  x: number;
+  y: number;
+};
+
+export type CursorData = {
+  x: number;
+  y: number;
+};
+
+export type Tile = {
+  uid: string;
+  x: number;
+  y: number;
+  name: string;
+  category: string;
+};
+
+export type MenuTileProps = {
+  name: string;
+  category: string;
+};
+
+export type InnerObject = {
+  category: string;
+  name: string;
+};
