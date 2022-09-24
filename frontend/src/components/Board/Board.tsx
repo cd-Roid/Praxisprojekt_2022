aaec58167a05cba5e2620587b7fb63395882b2f7
cd-Roid <giyawe98@gmail.com>
2022-09-25 01:08:03 +0200

    added Board background

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Tile from '../Tiles/Tile';
-import { Stage, Layer } from 'react-konva';
+import { Stage, Layer, Rect } from 'react-konva';
 import { useMouse } from '../../hooks/useMouse';
 import { Stage as StageType } from 'konva/lib/Stage';
 import { useBoardState } from '../../state/BoardState';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
+import img from '../../assets/images/board.jpg';
 
 // Main Stage Component that holds the Canvas. Scales based on the window size.
 
 const Board = () => {
-  const { height, width } = useWindowDimensions();
   const stageRef = React.useRef<StageType>(null);
+  const { height, width } = useWindowDimensions();
   const tilesOnBoard = useBoardState((state) => state.tilesOnBoard);
   const setStageReference = useBoardState((state) => state.setStageReference);
   setStageReference(stageRef);
@@ -20,6 +21,7 @@ const Board = () => {
     <main onDrop={(e) => handleDrop(e)} onDragOver={handleDragOver}>
       <div>
         <Stage
+          style={{ backgroundImage: `url(${img})` }}
           width={width}
           height={height}
           ref={stageRef}
