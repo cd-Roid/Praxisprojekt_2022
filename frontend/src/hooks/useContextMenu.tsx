7f0e96ff7c819dc8cb6ca736d76aaa11a09af220
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-07 00:14:05 +0200

    added custom Context Menu

---
+++ frontend/src/hooks/useContextMenu.tsx
@@ -0,0 +1,32 @@
+import { KonvaEventObject } from 'konva/lib/Node';
+import { useCallback } from 'react';
+import { useBoardState } from '../state/BoardState';
+
+export const useContextMenu = () => {
+  const contextMenu = useBoardState((state) => state.contextMenuOpen);
+  const setContextMenu = useBoardState((state) => state.setContextMenu);
+  const contextMenuAnchorPoint = useBoardState((state) => state.contextMenuAnchorPoint);
+  const setContextMenuAnchorPoint = useBoardState((state) => state.setContextMenuAnchorPoint);
+
+  const handleContextMenu = useCallback(
+    (event: KonvaEventObject<PointerEvent>) => {
+      event.evt.preventDefault();
+      setContextMenu(true);
+      if (event.target.parent?.id()) {
+        setContextMenuAnchorPoint({
+          x: event.evt.pageX,
+          y: event.evt.pageY,
+          id: event.target.parent.id(),
+        });
+      }
+    },
+    [setContextMenu],
+  );
+
+  const handleClick = useCallback(
+    () => (contextMenu ? setContextMenu(false) : null),
+    [contextMenu],
+  );
+
+  return { contextMenu, handleContextMenu, handleClick, contextMenuAnchorPoint };
+};
