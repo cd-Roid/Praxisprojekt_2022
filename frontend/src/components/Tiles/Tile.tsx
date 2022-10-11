a1e11f23cf745d4794b404487130e370abad84b6
cd-Roid <giyawe98@gmail.com>
2022-10-11 20:06:05 +0200

    removed unused code

--- frontend/src/components/Tiles/Tile.tsx
+++ frontend/src/components/Tiles/Tile.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { Tile as TileProps } from '../../types';
-import { Group as GroupType } from 'konva/lib/Group';
 import { Group, Text, Line } from 'react-konva';
+import { Group as GroupType } from 'konva/lib/Group';
 import { getTileType } from '../../hooks/useCategory';
 import { useMouse } from '../../hooks/useMouse';
 
 const Tile: React.FC<TileProps> = ({ name, x, y, category, uid }) => {
+  const { updateTilePosition } = useMouse();
   const tileRef = React.useRef<GroupType>(null);
   const { fill, points, text } = getTileType(category);
-  const { handleClick, updateTilePosition } = useMouse();
+  const { handleClick, setActiveDragElement } = useMouse();
+  /**
+     *     if (
+      tileRef.current &&
+      activeDragTile?.current &&
+      activeDragTile?.current?.id() !== tileRef.current.id() &&
+      activeDragTile.current.name() === 'Objects'
+    ) {
+      if (
+        activeDragTile?.current &&
+        Math.round(activeDragTile.current.x() - tileRef?.current?.x()) < snapXDistance &&
+        Math.round(activeDragTile.current.y() - tileRef?.current?.y()) >= -30 &&
+        Math.round(activeDragTile.current.y() - tileRef?.current?.y()) <= 30
+      ) {
+        const { width } = tileRef.current.getClientRect();
 
+        activeDragTile?.current?.to({
+          x: tileRef.current.x() + width,
+          y: tileRef.current.y(),
+          duration: 0.5,
+        });
+      }
+    } else {
+      return;
+    }
+    clearActiveDragTile();
+     */
   return (
     <>
       <Group
@@ -17,9 +43,10 @@ const Tile: React.FC<TileProps> = ({ name, x, y, category, uid }) => {
         draggable
         x={x}
         y={y}
-        onDragMove={(e) => updateTilePosition(e)}
         id={uid}
         name={category}
+        onDragMove={() => setActiveDragElement(tileRef)}
+        onDragEnd={updateTilePosition}
         onMouseOver={(e) => handleClick(e, 4)}
         onMouseLeave={(e) => handleClick(e, 0)}
       >
