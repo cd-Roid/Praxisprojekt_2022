f1fabf6de472f277efaa3786be8989d177c8b56e
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-18 21:15:51 +0200

    update cached Tiles

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,26 +11,68 @@ server.get("/", (req: express.Request, res: express.Response) =>
 	res.send("Hello from Server" + port),
 );
 
+export type CursorData = {
+	x: number;
+	y: number;
+	remoteUser: string;
+};
 
+export type NewNode = {
+	id: string;
+	category: string;
+	name: string;
+	x: number;
+	y: number;
+};
+
+export type SocketDragTile = {
+	remoteUser: string;
+	tile: NewNode;
+};
+
+const state = {
+	cachedTiles: [] as SocketDragTile[],
+	users: [] as string[],
+};
 
 io.on("connection", (socket) => {
-	console.log(`a user connected ${socket.id}`);
-	socket.on("new", (data) => {
-		io.emit("new", data, () => console.log("Resent event"));
-		console.log(data);
+	state.users.push(socket.id);
+	console.log(`a user connected ${socket.id}, users: ${state.users.length} `);
+	if (state.cachedTiles.length > 0) {
+		socket.emit("board-content", state.cachedTiles);
+	}
+	socket.on("tile-drop", (data: SocketDragTile) => {
+		state.cachedTiles.push(data);
+		socket.broadcast.emit("tile-drop", data);
 	});
-	socket.on("tile-drop", (data) => {
-		io.emit("tile-drop", data);
+	socket.on("tile-drag", (data: SocketDragTile) => {
+		socket.broadcast.emit("tile-drag", data);
+		state.cachedTiles.forEach((cachedTile) => {
+			if (cachedTile.tile.id === data.tile.id) {
+				cachedTile.tile.x = data.tile.x;
+				cachedTile.tile.y = data.tile.y;
+			}
+		});
 	});
-	socket.on("tile-drag", (data) => {
-		io.emit("tile-drag", data);
+	socket.on("cursor", (data: CursorData) => {
+		socket.broadcast.emit("cursor", data);
 	});
-	socket.on("cursor", (data) => {
-		io.emit("cursor", data);
+
+	socket.on("tile-delete", (data: string) => {
+		socket.broadcast.emit("tile-delete", data);
+		state.cachedTiles = state.cachedTiles.filter(
+			(tile) => tile.tile.id !== data,
+		);
 	});
 
 	socket.on("disconnect", () => {
-		console.log(`${socket.id} disconnected`);
+		state.users = state.users.filter((user) => user !== socket.id);
+		if (state.users.length === 0) {
+			console.log("cleared logs");
+			state.cachedTiles = [];
+			state.users = [];
+		}
+		console.log(`${socket.id} disconnected, users: ${state.users.length}`);
 	});
 
 	socket.on("error", (err) => {
