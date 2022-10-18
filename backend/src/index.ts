c2a5c7760912df3a840f1976c330ab200b2425e0
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-18 17:20:26 +0200

    added new events for dragging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,9 +19,13 @@ io.on("connection", (socket) => {
 		io.emit("new", data, () => console.log("Resent event"));
 		console.log(data);
 	});
-	socket.on("node-dragging", (data) => {
-		io.emit("node-dragging", data);
-		console.log(data);
+	socket.on("tile-drop", (data) => {
+		io.emit("tile-drop", data);
+		console.log("tile-drop", data);
+	});
+	socket.on("tile-drag", (data) => {
+		io.emit("tile-drag", data);
+		console.log("tile-drag", data);
 	});
 	socket.on("cursor", (data) => {
 		io.emit("cursor", data);
@@ -31,7 +35,6 @@ io.on("connection", (socket) => {
 	socket.on("disconnect", () => {
 		console.log(`${socket.id} disconnected`);
 	});
-	
 });
 
 io.listen(9001);
