78bb50943772db320d3e5045c33c5a953fc3edad
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-20 18:41:19 +0200

    added env file for ports

--- frontend/src/hooks/useWebSockets.tsx
+++ frontend/src/hooks/useWebSockets.tsx
@@ -2,8 +2,11 @@ import { useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 export const useWebSockets = () => {
+  const port = process.env.REACT_APP_PORT;
   const socketRef = useRef<Socket | null>(null);
-  const socket = io('http://localhost:9001', { transports: ['websocket'] });
+  const socket = io(`http://localhost:${port}`, {
+    transports: ['websocket'],
+  });
 
   useEffect(() => {
     socketRef.current = socket;
