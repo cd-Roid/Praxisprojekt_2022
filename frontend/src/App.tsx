9cc5a4ac7d8c18191f2b21eb3b3c783c2a9abdeb
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-23 21:43:25 +0200

    added router to App and 404 Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { useWebSockets } from './hooks/useWebSockets';
 import CanvasPage from './pages/CanvasPage';
 import LandingPage from './pages/LandingPage';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { useWebSocketState } from './state/WebSocketState';
+import NoMatch from './pages/NoMatch';
 
 const App = () => {
   // TODO: move the setting of the websocket to the LandingPage
@@ -13,7 +15,15 @@ const App = () => {
     socketRef.current && setSocket(socketRef.current);
   }, []);
 
-  return <CanvasPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/canvas' element={<CanvasPage />} />
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/NoMatch.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoMatch = () => {
+  return <div>Hier ist wohl nichts...</div>;
+};
+
+export default NoMatch;
