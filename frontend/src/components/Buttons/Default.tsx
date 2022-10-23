2e2676efc22a0b6565f2bc83e81495613ee0ee90
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-23 22:56:19 +0200

    made button generic and use in 404 page

---
+++ frontend/src/components/Buttons/Default.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type DefaultButtonProps = {
+  text: string;
+  onClick: () => void;
+  disabled?: boolean;
+  isDanger?: boolean;
+};
+
+const Default: React.FC<DefaultButtonProps> = ({ text, onClick, isDanger }) => {
+  return (
+    // Disconnect Button
+    <div
+      className={`${
+        isDanger ? 'bg-red-500 hover:bg-red-800' : 'bg-main hover:bg-dark'
+      } px-4 py-2 rounded-full text-white cursor-pointer`}
+      onClick={onClick}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default Default;
--- frontend/src/pages/NoMatch.tsx
+++ frontend/src/pages/NoMatch.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Default from '../components/Buttons/Default';
 
 const NoMatch = () => {
-  return <div>Hier ist wohl nichts...</div>;
+  const navigate = useNavigate();
+  const toHomePage = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className='w-screen h-screen '>
+      <div className='flex flex-col items-center p-24 justify-center h-full'>
+        <p className='text-h5 font-bold mb-24 tablet:text-h2 '>
+          Tut uns leid, da ist anscheinend was schief gelaufen.
+        </p>
+        <div className='w-fit'>
+          <Default text='Zur Homepage' onClick={toHomePage} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NoMatch;
