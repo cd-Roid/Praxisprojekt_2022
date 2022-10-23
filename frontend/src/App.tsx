e47db2de50cc90c524978efdc38127a308f0c390
George Iyawe <george.iyawe@kernpunkt.de>
2022-10-23 20:29:21 +0200

    refacotored websocket initiation to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react';
+import { useWebSockets } from './hooks/useWebSockets';
 import CanvasPage from './pages/CanvasPage';
 import LandingPage from './pages/LandingPage';
+import { useWebSocketState } from './state/WebSocketState';
 
 const App = () => {
+  // TODO: move the setting of the websocket to the LandingPage
+  const { socketRef } = useWebSockets();
+  const setSocket = useWebSocketState((state) => state.setSocket);
+
+  useEffect(() => {
+    socketRef.current && setSocket(socketRef.current);
+  }, []);
+
   return <CanvasPage />;
 };
 
--- frontend/src/pages/CanvasPage.tsx
+++ frontend/src/pages/CanvasPage.tsx
@@ -1,25 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Board from '../components/Board/Board';
 import { useToggle } from '../hooks/useToggle';
 import Sidebar from '../components/Sidebar/Sidebar';
 import AddTileForm from '../components/Forms/AddTileForm';
 import Cursor from '../components/Cursor/Cursor';
-import { useWebSockets } from '../hooks/useWebSockets';
 import { useWebSocketState } from '../state/WebSocketState';
 
 const CanvasPage = () => {
   const { isOpen, toggleForm } = useToggle();
-  const { socketRef } = useWebSockets();
-
-  const setSocket = useWebSocketState((state) => state.setSocket);
-
-  useEffect(() => {
-    socketRef.current && setSocket(socketRef.current);
-  }, []);
+  const socket = useWebSocketState((state) => state.socket);
 
   return (
     <>
-      {socketRef && <Cursor />}
+      {socket && <Cursor />}
       {isOpen && (
         <div className=' absolute bg-stone-600 w-full h-full'>
           <AddTileForm closeForm={() => toggleForm()} />
