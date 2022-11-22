0e586d5164abe9dfd77874df92605d4621284c84
George Iyawe <george.iyawe@kernpunkt.de>
2022-11-22 16:24:08 +0100

    styled cards correctly

--- admin-client/src/components/Tile.tsx
+++ admin-client/src/components/Tile.tsx
@@ -11,18 +11,16 @@ type TileProps = {
 
 const Tile: React.FC<TileProps> = ({ category, name, url, onClickFunc }) => {
   return (
-    <div className='m-8 w-72' onClick={onClickFunc}>
-      <div className='min-w-sm rounded overflow-hidden shadow-lg'>
-        <div className='mx-auto w-32 h-36 overflow-hidden cursor-pointer'>
-          <img src={url} alt='Mountain' />
-        </div>
-        <div className='px-6 py-4 cursor-pointer'>
-          <div className='font-bold text-xl mb-2'>{name}</div>
-          <p className='text-gray-700 text-base'>{category}</p>
-        </div>
-        <div className='px-6 pt-4 pb-2 flex justify-end items-center h-full w-full'>
-          <FontAwesomeIcon className='p-4 cursor-pointer text-black' icon={faTrash} />
-        </div>
+    <div className='m-8 w-56 h84 mt-16 mb-8 border border-black' onClick={onClickFunc}>
+      <div className='w-48 mt-8 mb-4 mx-4 h-36 overflow-hidden cursor-pointer '>
+        <img className='h-auto m-auto' src={url} alt={name} />
+      </div>
+      <div className='px-4 cursor-pointer'>
+        <div className='font-bold text-xl mb-2'>{name}</div>
+        <p className='text-gray-700 text-base'>{category}</p>
+      </div>
+      <div className=' flex justify-end items-center'>
+        <FontAwesomeIcon className='p-4 cursor-pointer text-black' icon={faTrash} />
       </div>
     </div>
   );
