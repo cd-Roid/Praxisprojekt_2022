5d7c66bfdccebcc2e18e2ce85c00d085e3d268df
George Iyawe <george.iyawe@kernpunkt.de>
2022-12-16 00:30:23 +0100

    removed external whitelist file and setup cors

--- backend/src/Config/originWhitelist.ts
+++
@@ -1,4 +0,0 @@
-//list of allowed origins
-const whitelist = ["http://localhost:3000", "http://localhost:3001"];
-
-export default whitelist;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,15 +47,26 @@ const io = new Server(server);
 const db = mongoose.connection;
 mongoose.connect(dbConfig.url + dbConfig.database);
 
-const corsOptions = {
-	origin: whiteList,
-};
 
+app.use((req, res, next) => {
+	const allowedOrigins = ["http://localhost:3001", "http://localhost:3000"];
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+		res.setHeader("Access-Control-Allow-Origin", origin);
+	}
+	//res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+	res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.header("Access-Control-Allow-Credentials", "true");
+	return next();
+});
 app.use(router);
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use(express.urlencoded({ extended: true }));
+
+
 
 io.on("connection", (socket) => {
 	socket.on("room-create", (data: UserData) => createRoom(data, state, socket));
