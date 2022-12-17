86011c14fe9785726cb4214079412dc763523ed8
George Iyawe <george.iyawe@kernpunkt.de>
2022-12-17 14:55:20 +0100

    added custom className variable

--- admin-client/src/components/Button.tsx
+++ admin-client/src/components/Button.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 type ButtonProps = {
   buttonText: string;
+  className?: string;
   clickFunction?: () => void;
 };
 
-const Button: React.FC<ButtonProps> = ({ buttonText, clickFunction }) => {
+const Button: React.FC<ButtonProps> = ({ buttonText, clickFunction, className }) => {
   return (
     <button
       onClick={clickFunction}
-      className='w-full text-center  py-2 text-white bg-main hover:bg-dark tablet:w-fit tablet:px-6'
+      className={`w-full text-center  py-2 text-white bg-main hover:bg-dark tablet:w-fit tablet:px-6 ${className}`}
     >
       {buttonText}
     </button>
