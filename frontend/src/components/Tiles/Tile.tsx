10e047fe62c1db7a25a012da73a3764de4c213a4
George Iyawe <george.iyawe@kernpunkt.de>
2023-01-22 21:51:09 +0100

    +1

--- frontend/src/components/Tiles/Tile.tsx
+++ frontend/src/components/Tiles/Tile.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Tile as TileProps } from '../../types';
-import { Group, Image } from 'react-konva';
+import { Group, Image, Text, Line } from 'react-konva';
 import { useMouse } from '../../hooks/useMouse';
 import { Group as GroupType } from 'konva/lib/Group';
 import { useContextMenu } from '../../hooks/useContextMenu';
-import useImage from 'use-image';
 import { useWebSocketState } from '../../state/WebSocketState';
-import { useBoardState } from '../../state/BoardState';
+// import useImage from 'use-image';
+// import { useBoardState } from '../../state/BoardState';
 
-const Tile: React.FC<TileProps> = ({ src, x, y, category, id }) => {
+const Tile: React.FC<TileProps> = ({
+  x,
+  y,
+  id,
+  src,
+  name,
+  color,
+  points,
+  category,
+  textPosition,
+}) => {
   const tileRef = React.useRef<GroupType>(null);
   const userColor = useWebSocketState(
     (state) => state.room?.users?.find((user) => user.userId === state.socket?.id)?.color,
   );
   const { handleMouseEnL, updateTilePosition, setActiveDragElement } = useMouse();
   const { handleContextMenu } = useContextMenu();
-  const [image] = useImage(src);
-  const remoteDragColor = useBoardState((state) => state.remoteDragColor);
+  // const [image] = useImage(src);
+  // const remoteDragColor = useBoardState((state) => state.remoteDragColor);
 
   // add border to image if active
 
@@ -37,13 +47,16 @@ const Tile: React.FC<TileProps> = ({ src, x, y, category, id }) => {
           onMouseOver={(e) => handleMouseEnL(e, 4)}
           onMouseLeave={(e) => handleMouseEnL(e, 0)}
         >
-          <Image
+          {/* <Image
             image={image}
             offsetX={image ? image.width / 2 : 0}
             offsetY={image ? image.height / 2 : 0}
             stroke={remoteDragColor ? remoteDragColor : userColor}
             strokeWidth={4}
-          />
+          /> */}
+
+          <Line fill={color} stroke={color} closed={true} strokeWidth={0} points={points} />
+          <Text text={name} fontSize={18} strokeWidth={12} x={textPosition.x} y={textPosition.y} />
         </Group>
       )}
     </>
