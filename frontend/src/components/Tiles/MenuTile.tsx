293491a5cd69d4a693b61063b67e9b228b1dca87
George Iyawe <george.iyawe@kernpunkt.de>
2023-01-22 21:59:19 +0100

    Revert "use Shapes on board and png in Sidebar"
    This reverts commit e93c2ed80ff888e5ef8d76c20e0a6a0a7d3b49f6.

--- frontend/src/components/Tiles/MenuTile.tsx
+++ frontend/src/components/Tiles/MenuTile.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 type MenuProps = {
   name: string;
   category: string;
-  src: string;
+  svgPath: string;
+  fill: string;
+  svgRotate: number;
+  url: string;
   dragFunction: (event: React.DragEvent) => void;
 };
 
-const MenuTile: React.FC<MenuProps> = ({ src, name, category, dragFunction }) => {
+const MenuTile: React.FC<MenuProps> = ({ url, name, category, dragFunction }) => {
   return (
     <img
       data-name={name}
       data-class={category}
-      src={src}
+      src={url}
       draggable
       onDragStart={dragFunction}
       className=' m-2 object-contain'
